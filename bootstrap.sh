53310dbf625ecc049ebb06a95d16d2d607bbd7ab
Hyacinthe Cartiaux <hyacinthe.cartiaux@uni.lu>
2013-10-09 11:11:35 +0200

    bootstrap.sh: copy authorized_keys file

--- bootstrap.sh
+++ bootstrap.sh
@@ -76,9 +76,10 @@ $TARGET aptitude clean
 $TARGET rm /root/linux-image.deb
 
 # Copy config files
-cp files/ttyAMA0.conf   $CHROOT_PATH/etc/init/
-cp files/ntp.conf       $CHROOT_PATH/etc/ntp.conf
-cp files/resolv.conf    $CHROOT_PATH/etc/resolv.conf
+cp files/ttyAMA0.conf    $CHROOT_PATH/etc/init/
+cp files/ntp.conf        $CHROOT_PATH/etc/ntp.conf
+cp files/resolv.conf     $CHROOT_PATH/etc/resolv.conf
+cp files/authorized_keys $CHROOT_PATH/root/.ssh/
 
 # Remove getty on tty*
 sed -i '/getty 38400/d' $CHROOT_PATH/etc/inittab
